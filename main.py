0d5adea3236aa25f59d3ae71658369761c7a1e81
Mustafa <bulutmustafa.0134@gmail.com>
2024-07-26 22:18:07 +0300

    this func has been created for transactions listing and gettin choice from user

--- main.py
+++ main.py
@@ -10,3 +10,35 @@ def prnt_location(func):
         func(*param,**params)
     return inner
 #endregion
+
+#region #todo choosing
+@prnt_location
+def choosing():
+    transactions = ["create a folder" , "list of folder" , "create a file","go to up location ","enter the folder","out"]
+    def choices():
+        for index,choice in enumerate(transactions,1):
+            print(f"{index}-{choice}")
+        print("-"*20)
+
+    class choice:
+        choices()    
+        def __init__(self,choice):
+            self.choice = choice
+            if  not (self.choice in [1,2,3,4,5,6]):
+                raise ValueError
+
+    while True:
+        try:
+            inp = int(input("ENTER TRANSACTION: "))
+            choice_object = choice(inp)
+        except ValueError:
+            print("please enter clearly")
+            choices()
+        else:
+            break
+
+    return choice_object.choice
+
+#endregion
+
+
