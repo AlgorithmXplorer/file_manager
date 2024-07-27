edcd66cc0d1dfed6460cd8129fc36b516eaf2003
Mustafa <bulutmustafa.0134@gmail.com>
2024-07-27 21:58:49 +0300

    some bug in the choosing_func has been solved and new func about create a file has been written

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
+transactions_of_app = ["create a folder" , "list of folder" , "create a file","go to up location ","enter the folder","out"]
+files = ["text file .txt","word file .docx","excel file .xlsx","powerpoint file .pptx","pdf file .pdf","cancel"]
 
 #region #todo decorator func
-def prnt_location(func):
+def prnt_location_for_ret_func(func):
+    def inner(*param,**params):
+        print("-"*50)
+        loc = os.getcwd().center(50," ")
+        print(loc)
+        print("-"*50)
+        return func(*param,**params)
+    return inner
+
+def prnt_location_for_normal_func(func):
     def inner(*param,**params):
         print("-"*50)
         loc = os.getcwd().center(50," ")
@@ -9,12 +20,13 @@ def prnt_location(func):
         print("-"*50)
         func(*param,**params)
     return inner
+
 #endregion
 
 #region #todo choosing
-@prnt_location
-def choosing():
-    transactions = ["create a folder" , "list of folder" , "create a file","go to up location ","enter the folder","out"]
+@prnt_location_for_ret_func
+def choosing(transactions):
+     
     def choices():
         for index,choice in enumerate(transactions,1):
             print(f"{index}-{choice}")
@@ -24,26 +36,30 @@ def choosing():
         choices()    
         def __init__(self,choice):
             self.choice = choice
-            if  not (self.choice in [1,2,3,4,5,6]):
+            if  not (0 <= self.choice and  self.choice < len(transactions) ):
                 raise ValueError
 
-    while True:
-        try:
-            inp = int(input("ENTER TRANSACTION: "))
-            choice_object = choice(inp)
-        except ValueError:
-            print("please enter clearly")
-            choices()
-        else:
-            break
+    def inner():
+        while True:
+            try:
+                inp = int(input("ENTER TRANSACTION: "))
+                choice_object = choice(inp-1)
+            except ValueError:
+                print("please enter clearly".center(50,"*"))
+                choices()
+            else:
+                break 
+        
+        return choice_object.choice 
+    
+    return inner()
 
-    return choice_object.choice
 
 #endregion
 
 #region #todo creating a folder
 def create_a_folder():
-    @prnt_location
+    @prnt_location_for_normal_func
     def inner():
         name = input("name: ")
         os.mkdir(name)
@@ -55,10 +71,10 @@ def create_a_folder():
         else: 
             print("folder has been created")
             break
-
 #endregion
-#region #todo list od folders
-@prnt_location
+
+#region #todo list of folders
+@prnt_location_for_normal_func
 def listing():
     folders = []
     files = []
@@ -73,6 +89,32 @@ def listing():
     print("FİLES".center(30,"_"))
     for i in files:
         print(i)
-   
+
 #endregion
 
+#region #todo creating a file
+def creating_a_file():
+    num = choosing(files)
+    def create_file():
+        file_str = files[num].split(" ")[-1]
+        name = input("file name: ")
+        path = os.getcwd()
+        with open(f"{path}\{name}{file_str}","x",encoding="utf-8") as file:
+            pass
+
+    @prnt_location_for_normal_func
+    def inner():
+        while True: 
+            try: 
+                create_file()
+            except FileExistsError :
+                print("already there is a file same name so please enter different name")
+            else:
+                print("file has been created succesfully")
+                break
+    if files[num] != "cancel":
+        inner()
+    else:
+        pass
+
+#endregion
