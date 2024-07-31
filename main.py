da8514827d2df8851cb613e9a316ecd6fc42f86d
Mustafa <bulutmustafa.0134@gmail.com>
2024-08-01 00:47:19 +0300

    project has been finished right now but new some functions will come

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-transactions_of_app = ["create a folder" , "list of folder" , "create a file","go to up location ","enter the folder","out"]
-files = ["text file .txt","word file .docx","excel file .xlsx","powerpoint file .pptx","pdf file .pdf","cancel"]
+from datetime import datetime 
+transactions_of_app = ["create a folder" , "create a file" , "list of folders and files" ,"go to up location ","enter the folder","out"]
+files = ["text file .txt","word file .docx","excel file .xlsx","powerpoint file .pptx","cancel"]
 
 #region #todo decorator func
 def prnt_location_for_ret_func(func):
@@ -73,48 +74,142 @@ def create_a_folder():
             break
 #endregion
 
-#region #todo list of folders
-@prnt_location_for_normal_func
-def listing():
-    folders = []
-    files = []
-    for i in os.listdir():
-        if "." in i :
-            files.append(i)
-        else:
-            folders.append(i)
-    print("FOLDERS".center(30,"_"))
-    for i in folders:
-        print(i)
-    print("FİLES".center(30,"_"))
-    for i in files:
-        print(i)
-
-#endregion
-
 #region #todo creating a file
 def creating_a_file():
-    num = choosing(files)
-    def create_file():
-        file_str = files[num].split(" ")[-1]
+    def create_file(path,extension):
         name = input("file name: ")
-        path = os.getcwd()
-        with open(f"{path}\{name}{file_str}","x",encoding="utf-8") as file:
-            pass
+        if  os.path.exists(f"{name}{extension}") == True:
+            print("already there is a folder same name so please enter different name")
+            return 0
+        else:
+            with open(f"{path}/{name}{extension}","a",encoding="utf-8") as file:
+                pass
+            print("folder has been created")
+            return 1
+
 
     @prnt_location_for_normal_func
     def inner():
-        while True: 
-            try: 
-                create_file()
-            except FileExistsError :
-                print("already there is a file same name so please enter different name")
-            else:
-                print("file has been created succesfully")
-                break
-    if files[num] != "cancel":
-        inner()
-    else:
-        pass
+        num = choosing(files)
+        file = files[num].split(" ")[-1]
+        file_path = os.getcwd()
+        if file == "cancel":
+            pass
+        else:
+            number = create_file(file_path,file)
+            while not(number == 1):
+                number = create_file(file_path,file)
+            inner()
+            
+
+    inner() 
 
 #endregion
+
+#region #todo list of folders and files
+def listing(number):
+    """
+    ınput: ıf 1 
+    output: list of folders
+    ınput: ıf 2
+    output: list of files
+    ınput: ıf 3
+    output: both of them
+    
+    """
+    @prnt_location_for_normal_func
+    def inner():
+        folders = []
+        files = []
+        for i in os.listdir():
+            if "." in i :
+                files.append(i)
+            else:
+                folders.append(i)
+
+        def list_of_folders():
+            print("FOLDERS".center(30,"_"),end="\n\n")
+            for i in folders:
+                file_info = os.stat(i)
+                second_of_create_date = file_info.st_ctime
+                just_date = datetime.fromtimestamp(second_of_create_date)
+                modded_date = datetime.strftime(just_date,"%x")
+                print(f"{i}\t\t\t\tcreate date: {modded_date}")
+
+        def list_of_files():
+            print("FİLES".center(30,"_"),end="\n\n")
+            for i in files:
+                file_info = os.stat(i)
+                second_of_create_date = file_info.st_ctime
+                just_date = datetime.fromtimestamp(second_of_create_date)
+                modded_date = datetime.strftime(just_date,"%x")
+                print(f"{i}\t\t\t\tcreate date: {modded_date}")
+    
+        if number == 1:
+            list_of_folders()
+            print("-"*70)
+        elif number == 2:
+            list_of_files()
+        elif number == 3:
+            list_of_folders()
+            list_of_files()
+        else:
+            raise ValueError("wrong number")
+    inner()
+#endregion
+
+#region #todo move up
+def move_up():
+    os.chdir("..")
+    def inner():
+        pass
+    inner()
+
+#endregion
+
+#region #todo entering to a folder
+def enter_folder():
+    def enter_folder(folder_name):
+        path = f"{os.getcwd()}/{folder_name}"
+        os.chdir(path)
+    
+    def inner():
+        listing(1)
+
+        folder_name = input("folder name: ")
+        if os.path.isdir(folder_name) == True:
+            enter_folder(folder_name)
+            
+        else:
+            print("there is not a folder with this name. please try again")
+            inner()
+    inner()
+
+#endregion
+# @prnt_location_for_normal_func
+def main():
+    choice = choosing(transactions_of_app)
+    if choice == 0:
+        create_a_folder()
+        main()
+
+    elif choice == 1:
+        creating_a_file()
+        main()
+
+    elif choice == 2:
+        listing(3)
+        main()
+    
+    elif choice == 3:
+        move_up()
+        main()
+
+    elif choice == 4:
+        enter_folder()
+        main()
+
+    elif choice == 5:
+        print("exited")
+
+main()
